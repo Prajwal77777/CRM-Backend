6d8d0496aaeff836e7cc1d0820e8438b34f4ce90
Prajwal <prajwal.khatri@gurzu.com>
2024-12-02 21:15:37 +0545

    feat: Add server and configuration completion

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,34 @@
 import express from "express"
+import compression from "compression";
+import bodyParser from "body-parser";
+import flash from "express-flash"
+import lusca from "lusca";
+
+
 
 const app = express();
+
+/*
+Express Configuration
+*/
+app.set("port",process.env.PORT || 3000);
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(flash());
+app.use(lusca.xframe("SAMEORIGIN"))
+app.use(lusca.xssProtection(true))
+
+/*
+Route health check
+*/
+app.get("/health_check",(req,res)=>{
+    res.status(200).json({status:"OK", message: "Your APi Health is good"})
+})
+
+
+
+
 
 
 export default app
